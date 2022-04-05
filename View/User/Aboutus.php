eb091ce61eed454f821579c5a5c1aea7664543b6
THUHAI1010 <thuhainguyen1010@gmail.com>
2022-04-05 22:52:59 +0700

    first commit

---
+++ View/User/Aboutus.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<section class="our-webcoderskull padding-lg">
+  <div style="background-image: url('Images/168419.jpg')" class="container">
+    <div class="row heading heading-icon">
+        <h2 style="font-family: tahoma; font-weight: bold; color: #fff">My Team</h2>
+    </div>
+    <ul style="margin-left: 200px;" class="row">
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="https://scontent.fsgn8-2.fna.fbcdn.net/v/t1.6435-9/152917487_434293794444383_3260050320684860492_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=m5Gv5CqS2aoAX8kBYN9&_nc_ht=scontent.fsgn8-2.fna&oh=7bb109377cbdd68613fac546647e33cb&oe=61C7D1CF" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/duykhavdk">Võ Duy Kha</a></h3>
+            <p style="font-weight: bold;color: #ff6600; font-size: 16px;">4501104104</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/duykhavdk"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://instagram.com/?lang=vi"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="https://f6.photo.talk.zdn.vn/9103182089487482230/552181d65afa91a4c8eb.jpg" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/huyem040701">Nguyễn Văn Huy Em</a></h3>
+            <p style="font-weight: bold;color: #ff6600; font-size: 16px">4501104059</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/huyem040701"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://instagram.com/?lang=vi"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t1.6435-9/69921163_678781269301627_1519914047594561536_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=KlD45TaOxb4AX9daiAG&_nc_ht=scontent.fsgn13-2.fna&oh=fc4b9556a00aa92ca1ab906919639dac&oe=61CABC79" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/hailyly.10102001">Nguyễn Thị Thu Hải</a></h3>
+            <p style="font-weight: bold;color: #ff6600; font-size: 16px">4501104068</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/hailyly.10102001"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://instagram.com/?lang=vi"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+       </li>
+    </ul>
+  </div>
+</section>
